bf13b0ed351dabe448d3c55629221ccd8a022964
Aditya Sharma <adityapandit264@gmail.com>
2025-02-26 20:26:48 +0530

    Create maxVal.js

---
+++ maxVal.js
@@ -0,0 +1,27 @@
+//!st Method using for loop
+let arr = [10,20,5,8,28]
+// let max=arr[0]
+// for(let i=0 ;i<arr.length;i++){
+   
+//     if(max<arr[i]){
+//         max=arr[i]
+//     }
+// }
+
+
+//2nd Method using reduce
+let max = arr.reduce((acc,curr)=>{
+    return acc>curr?acc:curr 
+    },arr[0])
+   
+  acc=10 , curr10
+  acc=10, curr=20
+ acc=20,curr=5
+acc=20,curr=8
+acc=20 curr=28
+acc=28
+
+
+//3rd Method using Math.max
+// const max = Math.max(...arr);
+// console.log(max); 
